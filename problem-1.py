5ebc4a1108715024a87687a9ac2368cb1ebf2d87
Gaurav Srivastava <64201825+gaurav-hub319@users.noreply.github.com>
2021-07-17 02:11:07 +0530

    Add files via upload

---
+++ problem-1.py
@@ -0,0 +1,16 @@
+#Problem number 1 -Given an integer, convert it to a roman numeral
+#Solution:-
+def convertRoman(n):
+    #Code here
+    arr = [[5000, 'V'],[4000, 'XV'],[3000, 'MMM'],[2000, 'MM'],[1000, 'M'],[900, 'CM'],[500, 'D'],[400, 'CD'],[100, 'C'],[90, 'XC'],[50, 'L'],[40, 'XL'],[10, 'X'], [9, 'IX'], [8, 'VIII'],[7, 'VII'],[6,'VI'],[5,'V'], [4,'IV'], [3,'III'],[2, 'II'],[1, 'I']]
+    s= ''
+    a = 0
+    while n!=0:
+        for i in range (len(arr)):
+            if n>=arr[i][0] :
+                s +=arr[i][1]
+                n-= arr[i][0]
+                break
+    return s
+n=int(input())
+print(convertRoman(n))
